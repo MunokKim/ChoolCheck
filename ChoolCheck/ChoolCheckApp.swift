a5dfa6f8461691c90fb4a094494c0da5b1cba0fb
Eli <wer0222@naver.com>
2023-10-14 00:00:05 +0900

    Initial Commit

---
+++ ChoolCheck/ChoolCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChoolCheckApp.swift
+//  ChoolCheck
+//
+//  Created by Eli_01 on 2023/10/13.
+//
+
+import SwiftUI
+
+@main
+struct ChoolCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
